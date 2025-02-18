552783e45c79f75e33ba135a7e851935679df7c5
Preetham Praveen <preethampraveencode26@gmail.com>
2025-02-18 14:51:26 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -39,6 +39,4 @@ app.get("/ping",(req,res)=>{
 app.listen(3000,()=>{
     console.log(`Server is running on port ${3000}`)
 })
-app.listen(3000,()=>{
-    console.log(`Server is running on port ${3000}`)
-})
+
