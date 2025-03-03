b6b338ab511676aa5c8fae5bf6f418118511c46c
Preetham Praveen <preethampraveencode26@gmail.com>
2025-03-03 15:05:17 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
